6da2de06731030105436965cbe8f39d8553ef15b
Roman Appenzeller <rappenze@yahoo.com>
2026-07-31 20:14:49 +0000

    Migrate fibaro integration to via_device_id

--- tests/components/fibaro/test_init.py
+++ tests/components/fibaro/test_init.py
@@ -2,15 +2,41 @@
 
 from unittest.mock import Mock, patch
 
+from homeassistant.components.fibaro import DOMAIN
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry as dr
 
-from .conftest import init_integration
+from .conftest import TEST_SERIALNUMBER, init_integration
 
 from tests.common import MockConfigEntry
 
 
-async def test_unload_integration(
+async def test_load_integration(
+    hass: HomeAssistant,
+    device_registry: dr.DeviceRegistry,
+    mock_fibaro_client: Mock,
+    mock_config_entry: MockConfigEntry,
+    mock_light: Mock,
+    mock_room: Mock,
+) -> None:
+    """Test load integration sets via device id for a light device to the hub device."""
+    # Arrange
+    mock_fibaro_client.read_rooms.return_value = [mock_room]
+    mock_fibaro_client.read_devices.return_value = [mock_light]
+
+    with patch("homeassistant.components.fibaro.PLATFORMS", [Platform.LIGHT]):
+        # Act
+        await init_integration(hass, mock_config_entry)
+        # Assert
+        hub_device = device_registry.async_get_device({(DOMAIN, TEST_SERIALNUMBER)})
+        light_device = device_registry.async_get_device(
+            {(DOMAIN, mock_light.fibaro_id)}
+        )
+        assert hub_device.id == light_device.via_device_id
+
+
+async def test_via_device_id(
     hass: HomeAssistant,
     mock_fibaro_client: Mock,
     mock_config_entry: MockConfigEntry,
